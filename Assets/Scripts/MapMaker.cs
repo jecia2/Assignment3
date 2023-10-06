671a6b11f360709bd3ac2b8376f88aecbed4e0f8
Jecia Robertson <jecia.a.robertson@student.uts.edu.au>
2023-10-06 13:54:29 +1100

    tween scripts

---
+++ Assets/Scripts/MapMaker.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapMaker : MonoBehaviour
+{
+    public GameObject outCorner;
+    public GameObject inCorner;
+    public GameObject inWall;
+    public GameObject outWall;
+    public GameObject tJunct;
+    int[,] levelMap =
+ {
+ {1,2,2,2,2,2,2,2,2,2,2,2,2,7},
+ {2,5,5,5,5,5,5,5,5,5,5,5,5,4},
+ {2,5,3,4,4,3,5,3,4,4,4,3,5,4},
+ {2,6,4,0,0,4,5,4,0,0,0,4,5,4},
+ {2,5,3,4,4,3,5,3,4,4,4,3,5,3},
+ {2,5,5,5,5,5,5,5,5,5,5,5,5,5},
+ {2,5,3,4,4,3,5,3,3,5,3,4,4,4},
+ {2,5,3,4,4,3,5,4,4,5,3,4,4,3},
+ {2,5,5,5,5,5,5,4,4,5,5,5,5,4},
+ {1,2,2,2,2,1,5,4,3,4,4,3,0,4},
+ {0,0,0,0,0,2,5,4,3,4,4,3,0,3},
+ {0,0,0,0,0,2,5,4,4,0,0,0,0,0},
+ {0,0,0,0,0,2,5,4,4,0,3,4,4,0},
+ {2,2,2,2,2,1,5,3,3,0,4,0,0,0},
+ {0,0,0,0,0,0,5,0,0,0,4,0,0,0},
+ };
+    /*0 – Empty (do not put anything here, no sprite needed)
+1 - Outside corner (double lined corner piece in original game)
+2 - Outside wall (double line in original game)
+3 - Inside corner (single lined corner piece in original game)
+4 - Inside wall (single line in original game)
+5 – An empty space with a Standard pellet (see Visual Assets above)
+6 – An empty space with a Power pellet (see Visual Assets above)
+7 - A t junction piece for connecting with adjoining regions*/
+    // Start is called before the first frame update
+    void Start()
+    {
+       /* outCorner.SetActive(false);
+        inCorner.SetActive(false);
+        outWall.SetActive(false);
+        inWall.SetActive(false);
+        tJunct.SetActive(false); */
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tweener.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweener : MonoBehaviour
+{
+    private Tween activeTween;
+
+    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (activeTween == null)
+        {
+            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+            Debug.Log("Tween here");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activeTween != null)
+        {
+            float targetDist = Vector3.Distance(activeTween.Target.position, activeTween.EndPos);
+            //divide elapsed time by duration
+            float timeStuff = (Time.time - activeTween.StartTime) / activeTween.Duration;
+            float percentage = timeStuff / targetDist;
+            //Debug.Log(percentage);
+            Debug.Log("yippee");
+            if (targetDist > 0.1f)
+            {
+                activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, percentage);
+            }
+            else if (targetDist <= 0.1f)
+            {
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+            }
+        }
+
+    }
+}
